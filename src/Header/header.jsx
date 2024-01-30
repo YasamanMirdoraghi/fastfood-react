9e54c7723641cc4973f115ba83ef037fc7595ab6
Yasaman <125014861+YasamanMirdoraghi@users.noreply.github.com>
2024-01-30 15:44:05 +0330

    developed by yasaman

---
+++ src/Header/header.jsx
@@ -0,0 +1,14 @@
+import './header.css';
+const Header = () => {
+    return (
+        <header className='py-5'>
+            <div className="container">
+                <h1 className='text-start text-light text-uppercase'>
+                    fast food catalogue
+                </h1>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
